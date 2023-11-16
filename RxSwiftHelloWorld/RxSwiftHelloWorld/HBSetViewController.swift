6ffe3ff977a3e667d188cda85bfcb2c5e0de89d9
gmy <>
2023-11-16 10:10:48 +0800

    RxSwift dynamic refresh

--- RxSwiftHelloWorld/RxSwiftHelloWorld/HBSetViewController.swift
+++ RxSwiftHelloWorld/RxSwiftHelloWorld/HBSetViewController.swift
@@ -34,6 +34,7 @@ class HBSetViewController: UIViewController {
     )
     let setViewModel = HBSetViewModel()
 //    var items: Observable<Array<SectionModel<String, HBSetCellModel>>>?
+    var dataList: BehaviorSubject<Array<SectionModel<String, HBSetCellModel>>>?
     var idCardStatus: String?
     // MARK: root view life circles
     override func viewDidLoad() {
@@ -42,13 +43,16 @@ class HBSetViewController: UIViewController {
         title = "设置"
         view.addSubview(tableView)
         
-        var dataList = BehaviorSubject(value: [SectionModel<String,Any>]())
-        
+//        var dataList = BehaviorSubject(value: [SectionModel<String,HBSetCellModel>]())
+        dataList = BehaviorSubject(value: setViewModel.sectionModels)
         let dataSource = self.dataSource
-        let items = Observable.just(setViewModel.sectionModels)
-        items
+        dataList?.asObserver()
             .bind(to: tableView.rx.items(dataSource: dataSource))
             .disposed(by: disposeBag)
+//        let items = Observable.just(setViewModel.sectionModels)
+//        items
+//            .bind(to: tableView.rx.items(dataSource: dataSource))
+//            .disposed(by: disposeBag)
         tableView.rx
             .itemSelected
             .map { indexPath in
@@ -75,7 +79,7 @@ class HBSetViewController: UIViewController {
             return
         }
         setViewModel.updateIDCardState(idCardStatus)
-//        items = Observable.just(setViewModel.sectionModels)
+        dataList?.onNext(setViewModel.sectionModels)
     }
 }
 
